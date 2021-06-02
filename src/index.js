a04d31b7bd80a89a77c5ad8dff0206cc40558601
Rohan Arya <rohan.arya237@gmail.com>
2021-06-02 09:17:51 +0530

    ~

--- src/index.js
+++ src/index.js
@@ -1,42 +1,56 @@
-console.log("Hello webpack!");
+import 'regenerator-runtime/runtime'
 
-const fancyFunc = () => {
-  console.log('inside funct');
-  return [1, 2];
-};
-
-// document.getElementById('button').onclick = () =>{
-//   console.log('button clicked');
-// }
-
-const [a, b] = fancyFunc();
-console.log(a, b);
-
-
-class SampleComponent extends HTMLElement {
-
+window.addEventListener('load',()=>{
+  fetchpoke();
   
-  constructor() {
-    super(); 
-    shadowRoot = this.attachShadow({mode: 'open'});
-    const demo = `
-    <h1>Hello webpack!</h1>
-    <button id="button">Click me</button>
+});
+
+
+async function fetchpoke(){
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=20');
+  const json = await response.json();
+
+  //console.log(json.results);
+  //pokemons = json.results;
+
+  const main = document.querySelector('main')
+
+
+  json.results.forEach(element => {
+    const PokemonCard = document.createElement("poke-cards")
+    PokemonCard.element = element;
+    //console.log(element);
+    const details = element.url;
+
+    main.appendChild(PokemonCard);
+    fetch_ability();
+    async function fetch_ability(){
+      const response = await fetch(details);
+      const json = await response.json();
+      const ability = json.abilities;
+      console.log(ability[1])
+    }
+  });
+}
+
+
+class PokemonDetails extends HTMLElement{
+
+
+  set element(element){
+    this.innerHTML=`
+      <div>
+      <p>${element.name}</p>
+      <ul>
+      <li>${element.url}</li>
+      </ul>
+      </div>
+      
     `
-    this.render(demo)
   }
 
-
-  connectedCallback() {
-    this.shadowRoot.getElementById('button').onclick = () =>{
-        console.log('button clicked');
-      }
-  }
-
-  render(source){
-    this.shadowRoot.innerHTML = source
-  }
 
 }
 
-customElements.define('sample-comp', SampleComponent);
+
+customElements.define('poke-cards',PokemonDetails);
